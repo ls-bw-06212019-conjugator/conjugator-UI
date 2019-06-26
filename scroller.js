befa29390e3cccf15f27ad988d111a15d25d2c1b
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-06-26 14:35:15 -0700

    paste in my tabs code for reference

--- scroller.js
+++ scroller.js
@@ -1,1 +1,60 @@
 console.log('hello world')
+
+class Tabs {
+    constructor(element) {
+      this.element = element;
+  
+      this.tabLinks = {};
+      const linkElements = element.querySelectorAll('.tabs-link');
+      for (let i = 0; i < linkElements.length; i++) {
+        this.tabLinks[linkElements[i].dataset.tab] = new TabLink(linkElements[i], this);
+      }
+  
+      this.tabItems = {};
+      const itemElements = element.querySelectorAll('.tabs-item');
+      for (let i = 0; i < itemElements.length; i++) {
+        this.tabItems[itemElements[i].dataset.tab] = new TabItem(itemElements[i]);
+      }
+    }
+  
+    choose(tabID) {
+      Object.keys(this.tabLinks).forEach(key => this.tabLinks[key].deselect());
+      this.tabLinks[tabID].select();
+      Object.keys(this.tabItems).forEach(key => this.tabItems[key].deselect());
+      this.tabItems[tabID].select();
+    }
+  }
+  
+  class TabLink {
+    constructor(element, parent) {
+      this.element = element;
+      this.data = element.dataset.tab;
+      
+      this.element.addEventListener('click', () => parent.choose(this.data));
+    };
+  
+    select() {
+      this.element.classList.add('tabs-link-selected');
+    }
+  
+    deselect() {
+      this.element.classList.remove('tabs-link-selected');
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      this.element.classList.add('tabs-item-selected');
+    }
+  
+    deselect() {
+      this.element.classList.remove('tabs-item-selected');
+    }
+  }
+  
+  const tabSections = document.querySelectorAll('div.tabs');
+  tabSections.forEach(divElement => new Tabs(divElement));
