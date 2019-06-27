a89720fa02e7e29929ba8462a7770efe30860cde
Emily Lagattolla <elagattolla@gmail.com>
2019-06-27 07:30:04 -0500

    WIP: accordion.js; scaffolding eventListener

--- accordion.js
+++ accordion.js
@@ -1,1 +1,31 @@
-class PanelsLink
+class PanelsLink {
+  constructor(accordionElement) {
+    this.accordionElement = accordionElement;
+    this.panelData = this.accordionElement.dataset.tense;
+    this.itemElement = document.querySelector(`.panel[data-tense='${this.panelData}']`);
+    this.panelElement = new PanelItem(this.panelElement);
+    this.element.addEventListener('click', () => this.select())
+  }
+
+  select() {
+
+  }
+}
+
+class PanelItem {
+  constructor(accordionElement) {
+    // Assign this.element to the passed in element
+    this.element = accordionElement;
+  }
+
+  select() {
+    // Select all ".tabs-item" elements from the DOM
+    const items = document.querySelectorAll('.tabs-item');
+
+    // Remove the class "tabs-item-selected" from each element
+    Array.from(items).forEach(element => element.classList.remove('tabs-item-selected'));
+
+    // Add a class named "tabs-item-selected" to this element
+    this.element.classList.add('tabs-item-selected');
+  }
+}
