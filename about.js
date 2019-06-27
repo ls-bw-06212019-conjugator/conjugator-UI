eacf6688189b0e523bf279fb9944e733484d9ebe
Margaret <mpbeck@umich.edu>
2019-06-27 17:14:04 -0400

    working java

--- about.js
+++ about.js
@@ -3,21 +3,22 @@
 class Feature {
     constructor(domElement) {
       this.domElement = domElement;
-      
+
       this.expandButton = this.domElement.querySelector('.expandButton');
-     
-      this.expandButton.textContent = "Read More";
-      
-      
-      this.expandButton.addEventListener('click', () => this.expandFeature(this));
-    }
-      
-    
-  
+
+      this.paragraphtext = this.domElement.querySelector('.paragraphtext');
+
+
+      this.expandButton.addEventListener('click', () => this.expandFeature());
+    } 
+
     expandFeature() {
       
-      this.domElement.classList.toggle('feature-open');
+      this.paragraphtext.classList.toggle('paragraphtext-open');
+    console.log('callsuccesful')
+
     } 
+
 }
 
   
